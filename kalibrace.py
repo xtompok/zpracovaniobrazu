a8eb1a57f0f2c040dbb7d053f5e22c6c1f9b037d
xtompok <xtompok@c3a34024-e514-11de-a21f-59b1e5349453>
2010-02-05 07:26:52 +0000

    Deleted Python code, maybe all.

--- kalibrace.py
+++
@@ -1,7 +0,0 @@
-from PIL import Image,ImageDraw
-
-def kobr(x,y, r=10, w=800, h=600):
-  i = Image.new('RGB', (w,h))
-  d = ImageDraw.Draw(i)
-  d.rectangle((x-r/2, y-r/2, x+r/2, y+r/2), fill=(255, 0, 0))
-  return i
